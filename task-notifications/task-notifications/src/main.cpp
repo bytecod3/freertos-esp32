c31ecae87e0cf49a6407d5e25d29d7dd504fb20c
bytecod3 <emwiti658@gmail.com>
2025-11-18 07:31:06 +0300

    ADDS a simple task notification from ISR

--- task-notifications/task-notifications/src/main.cpp
+++ task-notifications/task-notifications/src/main.cpp
@@ -4,9 +4,11 @@
 /**
  * task handles 
  */
-TaskHandle_t x_task_consume;
-TaskHandle_t x_task_produce;
-TaskHandle_t v_handler_task;
+TaskHandle_t x_task_consume_handle;
+TaskHandle_t x_task_produce_handle;
+TaskHandle_t v_handler_task_handle;
+
+TimerHandle_t my_isr_timer;
 
 /* rate for interrut generation */
 const TickType_t x_interrupt_freq = pdMS_TO_TICKS(500UL);
@@ -18,23 +20,48 @@ void v_handler_task(void* pv_parameters) {
 
   for(;;) {
 
-    events_to_process = ulTaskNotifyTake(pdTRUE, expected_block_time); /* wait for a task notification from ISR */
+    /* wait for a task notification from ISR */
+    ulTaskNotifyTake(pdTRUE, expected_block_time); 
+    Serial.println("Software interrupt triggered");
 
-    if(events_to_process != 0){ /* at least one event must have occured to get here */
-      while(events_to_process > 0) {
-        Serial.printf("Handler task - processing event.\r\n");
-        events_to_process--;
-      }
-    } else {
-      /* interrupt did not arrive within thw wxpected time - perfom error recovery */
-    }
+    // if(events_to_process != 0){ /* at least one event must have occured to get here */
+    //   while(events_to_process > 0) {
+    //     Serial.printf("Handler task - processing event.\r\n");
+    //     events_to_process--;
+    //   }
+    // } else {
+    //   /* interrupt did not arrive within thw wxpected time - perfom error recovery */
+    // }
+
   }
 }
 
-void setup() {
-  Serial.println(115200);
+static void IRAM_ATTR ul_example_interrupt_handler(TimerHandle_t x_timer) {
+  BaseType_t x_higher_priority_task_woken;
+  x_higher_priority_task_woken = pdFALSE;
 
+  /* send notification directly to the task to which the interrupt is being deferred */
+  vTaskNotifyGiveFromISR(v_handler_task_handle, &x_higher_priority_task_woken);
+
+  /* check for a context switch */
+  portYIELD_FROM_ISR(&x_higher_priority_task_woken);
+
+}
+
+void setup() {
+  Serial.begin(115200);
   delay(400);
+
+  if(xTaskCreate(v_handler_task, "v_task_handler", 1024, NULL, 1, &v_handler_task_handle)) {
+    Serial.println("V task handler created OK");
+  } else {
+    Serial.println("V task handler failed to create");
+  }
+
+  /* create timer to periodically call the ISR */
+  my_isr_timer = xTimerCreate("myTimer", pdMS_TO_TICKS(1000), pdTRUE, NULL, ul_example_interrupt_handler);
+  xTimerStart(my_isr_timer, 0);
+
 }
 
 void loop() {
